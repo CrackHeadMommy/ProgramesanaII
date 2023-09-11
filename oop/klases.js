87a31de6fe3ade29d33a860929092ad9c633a22b
CrackHeadMommy <112924655+CrackHeadMommy@users.noreply.github.com>
2023-09-11 14:39:45 +0300

    holy moly

--- oop/klases.js
+++ oop/klases.js
@@ -3,10 +3,28 @@ class Cilveks {
         this.vecums = age;
         this.vards = name;
         this.dzimums = sex;
+
+        let rezultataVieta = document.getElementById("rezultats")
+
+        let cilvekaDiv = document.createElement("div")
+        this.infoVieta = document.createElement("p")
+        cilvekaDiv.appendChild(this.infoVieta)
+
+        let dzimsanasdienasPoga = document.createElement("button")
+        dzimsanasdienasPoga.innerHTML = "Dzimšanas Diena!"
+        dzimsanasdienasPoga.onclick = () => this.svinetDzD();
+
+        cilvekaDiv.appendChild(dzimsanasdienasPoga)
+        rezultataVieta.appendChild(cilvekaDiv)
+
+
+
+
         this.info();
     }
     svinetDzD(){
         this.vecums ++;
+        this.info();
     }
     mainitVardu(jaunaisVards){
         this.vards = jaunaisVards;
@@ -35,9 +53,20 @@ class Cilveks {
             teksts += this.dzimums;
         }
         console.log(teksts)
+        this.infoVieta.innerHTML = teksts
     }
 
 
 }
 
-pirmais = new Cilveks(15, "Katrīna", "s")
+// pirmais = new Cilveks(15, "Katrīna", "s")
+
+let visiCilveki = [];
+
+
+function izveidotCilveku(){
+    let vards = document.getElementById("vards").value
+    let dzimums = document.getElementById("dzimums").value
+    let vecums = document.getElementById("vecums").value
+    visiCilveki.push( new Cilveks(vecums, vards, dzimums))
+ }
