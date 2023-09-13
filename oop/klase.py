1066caa2a9d4e6760e49d0f51c6458bae96fa41d
CrackHeadMommy <112924655+CrackHeadMommy@users.noreply.github.com>
2023-09-13 09:57:30 +0300

    hihi
    l bozo

---
+++ oop/klase.py
@@ -0,0 +1,45 @@
+class Cilveks:
+    def __init__(self, name, age, sex):
+        self.vecums = age
+        self.vards = name
+        self.dzimums = sex
+        self.info()
+    
+    def svinet_dz_d(self):
+        self.vecums += 1
+        self.info()
+
+    def mainit_vardu(self, jaunais_vards):
+        self.vards = jaunais_vards
+        self.info()
+    
+    def mainit_dzimumu(self, jaunais_dzimums = ""):
+        if jaunais_dzimums == "":
+            if self.dzimums == "s":
+                self.dzimums = "v"
+            else:
+                self.dzimums = "s"
+        else:
+            self.dzimums = jaunais_dzimums
+            self.info()
+
+    def info(self):
+        print("Sveiki! Mani sauc ", self.vards, ".", sep = "")
+        print("Man ir", self.vecums, "gadu.")
+        if self.dzimums == "s":
+            print("Es esmu sieviete")
+        elif self.dzimums == "v":
+            print("Es esmu vīrietis")
+        else:
+            print("Es esmu ", self.dzimums, ".", sep = "")
+
+    def mirt_pavisam(self):
+        print("Visu labu! *nomirst*")
+        self.__del__()
+
+pirmais = Cilveks("Anna", 12, "v")
+
+pirmais.mainit_dzimumu("nekatrs")
+pirmais.mainit_vardu("Joe")
+pirmais.svinet_dz_d()
+pirmais.mirt_pavisam()
